fc0baef39cf8b7cd43b6a0d631fbaf149b79f913
Akshath <akshath.kaushal@gmail.com>
2019-11-25 12:20:34 +0530

    auto upload

--- project 3/imt2018501.cpp
+++ project 3/imt2018501.cpp
@@ -4,17 +4,18 @@
 
 /*
     Factorial code:
-    
-    addi $s0 $zero 5    //  001000  10000  00000  0000000000000101    //  i type
-    addi $s1 $zero 5    //  001000  10001  00000  0000000000000101    //  i type
+
+    lw   $s0 0($zero)   //  100011  10000  00000  0000000000000000    //  i type
+    lw   $s0 0($zero)   //  100011  10001  00000  0000000000000000    //  i type
     addi $t0 $zero 1    //  001000  01000  00000  0000000000000001    //  i type
     addi $t1 $zero 1    //  001000  01001  00000  0000000000000001    //  i type
 
     loop:
-    beq $s1 1 end       //  000100  10001  00001  0000000000000101    //  i type
+    beq $s1 1 end       //  000100  10001  00001  0000000000000100    //  i type
     sub $s1 $s1 $t0     //  000000  10001  10001  01000  00000  100010  // r type
     mul $s0 $s0 $s1     //  000000  10000  10000  10001  00000  011000  // r type
     j loop              //  000010  11111111111111111111111101          //  j type
+    sw  $s0 0($zero)   //  101011  10000  00000  0000000000000001    //  i type
 
     end:
 
@@ -23,21 +24,30 @@
 /*
     Instructions:
     
-    1) addi
-    2) subtract
-    3) mul
-    4) jump
-    5) beq
+    1) sw
+    2) lw
+    3) addi
+    4) subtract
+    5) mul
+    6) jump
+    7) beq
 */
 
+/*
+    Clocke cycles per instruction:
+
+    1) Load (5 cycles)
+    2) Store (4 cycles)
+    3) R-type (4 cycles)
+    4) Branch (3 cycles)
+    5) Jump (3 cycles)
+*/
 
 
 #include<bits/stdc++.h>
 #include<fstream>
 using namespace std;
-
-int clock_cycles=0;
-
+    
 vector<int> registerFile;      // register file
 vector<int> memory;            // main memory
 int pc=0;
@@ -78,7 +88,6 @@ int binToInt(string binary)
 
 int twosComplement(string address)
 {
-    // 111101
     for(int i=0;i<address.length();i++)
     {
         if(address[i]=='1')
@@ -114,7 +123,7 @@ char tellType(string instruction)
     else if(instruction.substr(0,6)=="000010") type = 'j';
     else type = 'i';
 	return type; // this will determine further action on the instruction
-}   // working fine
+}   
 
 // Seperate different fields according to the type of instruction
 vector<int> decodeInstruct(string instruction)
@@ -165,9 +174,19 @@ vector<int> decodeInstruct(string instruction)
     }
 
 	return fields;
-}   // working fine
+}   
  
 // interpreting instructions
+void sw(int rt, int rs, int offset)
+{
+    memory[rs+offset] = registerFile[rt];
+}
+
+void lw(int rt, int rs, int offset)
+{
+    registerFile[rt] = memory[rs+offset];
+}
+
 void addi(int rt, int rs, int val)
 {
     registerFile[rt] = registerFile[rs]+val;
@@ -201,12 +220,21 @@ void j(int offset)
 
 int main()
 {	
-    int check;
     // initializing memory and registers
     initialize();
+
+    // take input 
+    int input;
+    string swCode="";
+    cout<<"Enter a digit: ";
+    cin>>input;
+    memory[0]=input;
+
+    int clock_cycles=0;
+
     vector<string> instructions;   // instruction set
 
-    // reading from file
+    // reading instructions from file
     ifstream infile("instructions/factorial.txt");
     string inst;
     while(infile >> inst)
@@ -216,13 +244,28 @@ int main()
 
     int inst_count = instructions.size();
     
-    cout<<"PC"<<"\t"<<"INST"<<"\t"<<"$s0"<<"\t"<<"$s1"<<"\t"<<"$t0"<<"\t"<<"$t1"<<endl<<endl;
+    cout<<"PC"<<"\t"<<"INST"<<"\t"<<"$s0"<<"\t"<<"$s1"<<"\t"<<"$t0"<<"\t"<<"$t1"<<endl;
+    cout<<"-------------------------------------------"<<endl;
 
     while(pc<inst_count)
     {
         // call instructions here
         vector<int> components = decodeInstruct(instructions[pc]);
-        if(components[0]==binToInt("001000")){
+        if(components[0]==binToInt("101011"))
+        {
+            cout<<"pc="<<pc<<"\tsw"<<"\t";
+            sw(components[1], components[2], components[3]);
+            pc++;
+        }
+
+        else if(components[0]==binToInt("100011"))
+        {
+            cout<<"pc="<<pc<<"\tlw"<<"\t";
+            lw(components[1], components[2], components[3]);
+            pc++;
+        }
+
+        else if(components[0]==binToInt("001000")){
             cout<<"pc="<<pc<<"\taddi"<<"\t";
             addi(components[1], components[2], components[3]);
             pc++;
@@ -254,11 +297,9 @@ int main()
         }
 
         cout<<registerFile[16]<<"\t"<<registerFile[17]<<"\t"<<registerFile[8]<<"\t"<<registerFile[9]<<endl;
-
     }
 
-    cout<<"\nAnswer = "<<registerFile[16]<<endl;
-
+    cout<<"\nAnswer = "<<memory[1]<<endl;
     return 0;
 }
 
@@ -269,7 +310,6 @@ int main()
     2) Write the instructions in instructions.txt               Done
     3) Decode Instructions                                      Done
     4) Start interpreting                                       Done
-    5) Call the instructions in order of Factorial code         pending
-
-
+    5) Call the instructions in order of Factorial code         Done
+    6) count the number of clock cycles                         pending
 */
